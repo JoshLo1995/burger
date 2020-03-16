8895fbe36a8da7810a9ad8b95fd942c5f6c85d17
Joshua Lo <josh.lol1995@gmail.com>
2020-03-16 01:13:16 -0700

    filled out burger.js

---
+++ models/burger.js
@@ -0,0 +1,33 @@
+const orm = require('../config/orm.js');
+
+const burger = {
+    all: (callback) => {
+        orm.all("BURGERS", (res) => {
+            callback(res);
+        });
+    },
+
+    create: (cols, vals, callback) => {
+        orm.create("BURGERS", cols, vals, (res) => {
+            callback(res);
+        });
+    },
+
+    update: (objColVals, condition, callback) => {
+        orm.update("BURGERS", objColVals, condition, (res) => {
+            callback(res);
+        });
+    },
+
+    delete: (condition, callback) => {
+        orm.delete("BURGERS", condition, (res) => {
+            callback(res);
+        });
+    }
+};
+
+module.exports = burger;
+
+
+
+module.exports = burger;
